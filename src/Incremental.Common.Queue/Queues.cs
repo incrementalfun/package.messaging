4feefff7ed7f0ff02fdbb2c9238521d2b0fa839e
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-05 11:02:27 +0100

    Added new queue to reference

--- src/Incremental.Common.Queue/Queues.cs
+++ src/Incremental.Common.Queue/Queues.cs
@@ -1,13 +1,18 @@
 ﻿namespace Incremental.Common.Queue
 {
     /// <summary>
-    /// Queue reference.
+    /// Queue endpoints.
     /// </summary>
     public static class Queues
     {
         /// <summary>
-        /// Service Queue
+        /// Service Queue.
         /// </summary>
         public static readonly string Services = "https://sqs.eu-west-1.amazonaws.com/***REMOVED***/incremental_services.fifo";
+        
+        /// <summary>
+        /// Statistics Queue.
+        /// </summary>
+        public static readonly string Statistics = "https://sqs.eu-west-1.amazonaws.com/***REMOVED***/incremental_statistics.fifo";
     }
 }

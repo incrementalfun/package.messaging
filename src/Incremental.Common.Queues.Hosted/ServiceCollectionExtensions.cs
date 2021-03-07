f8da9c97cfe58629e55655eb5f38d8a5039f13ba
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-07 15:11:09 +0100

    Added DI

--- src/Incremental.Common.Queues.Hosted/ServiceCollectionExtensions.cs
+++ src/Incremental.Common.Queues.Hosted/ServiceCollectionExtensions.cs
@@ -1,8 +1,12 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Linq;
+using System.Reflection;
 using Amazon;
 using Amazon.Extensions.NETCore.Setup;
 using Amazon.Runtime;
 using Amazon.SQS;
+using Incremental.Common.Queues.Hosted.Hosted;
+using Incremental.Common.Queues.Hosted.Options;
 using Incremental.Common.Queues.Messages;
 using Incremental.Common.Queues.Messages.Contract;
 using Incremental.Common.Queues.Service;
@@ -19,9 +23,32 @@ namespace Incremental.Common.Queues.Hosted
     public static class ServiceCollectionExtensions
     {
         /// <summary>
-        /// TODO:
+        /// Registers a hosted service to handle incoming messages of a specific queue.
         /// </summary>
-        /// <returns></returns>
-        public static IServiceCollection AddQueuesHostedServices(this IServiceCollection services, )
+        /// <returns><see cref="IServiceCollection"/></returns>
+        public static IServiceCollection AddQueuesHostedServices(this IServiceCollection services, Action<CommonQueuesOptions> hostedOptions)
+        {
+            var options = new CommonQueuesOptions();
+            
+            hostedOptions.Invoke(options);
+
+            if (string.IsNullOrWhiteSpace(options.QueueEndpoint))
+            {
+                throw new ArgumentException("QueueEndpoint is a required argument.");
+            }
+
+            if (options.RegisteredMessageTypes.Any() && options.RegisteredMessageTypes.Values.All(t => t.IsAssignableTo(typeof(Message))))
+            {
+                
+                foreach (var registeredMessageType in options.RegisteredMessageTypes.Values)
+                {
+                    services.AddScoped(typeof(Message), registeredMessageType);
+                }
+
+                services.AddHostedService<QueueHostedService>();
+            }
+            
+            return services;
+        }
     }
 }

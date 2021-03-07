f5067c9d65ab4f0ff703dfc5dbad73ae68e63b1d
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-07 15:09:27 +0100

    Added hosted options

---
+++ src/Incremental.Common.Queues.Hosted/Options/CommonQueuesOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Incremental.Common.Queues.Hosted.Options
+{
+    public class CommonQueuesOptions
+    {
+        public string QueueEndpoint { get; set; }
+        public Dictionary<string, Type> RegisteredMessageTypes { get; set; }
+    }
+}

7503a0949adef5f22dffe9ad74f6560e91a97ac7
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-07 15:11:55 +0100

    Runned code cleanup

--- src/Incremental.Common.Queues.Hosted/Options/CommonQueuesOptions.cs
+++ src/Incremental.Common.Queues.Hosted/Options/CommonQueuesOptions.cs
@@ -4,17 +4,17 @@ using System.Collections.Generic;
 namespace Incremental.Common.Queues.Hosted.Options
 {
     /// <summary>
-    /// Common options for hosted queue service.
+    ///     Common options for hosted queue service.
     /// </summary>
     public class CommonQueuesOptions
     {
         /// <summary>
-        /// Queue endpoint.
+        ///     Queue endpoint.
         /// </summary>
         public string QueueEndpoint { get; set; }
-        
+
         /// <summary>
-        /// Types of messages that this queue will handle.
+        ///     Types of messages that this queue will handle.
         /// </summary>
         public Dictionary<string, Type> RegisteredMessageTypes { get; set; }
     }
--- src/Incremental.Common.Queues.Hosted/ServiceCollectionExtensions.cs
+++ src/Incremental.Common.Queues.Hosted/ServiceCollectionExtensions.cs
@@ -1,18 +1,8 @@
 ﻿using System;
 using System.Linq;
-using System.Reflection;
-using Amazon;
-using Amazon.Extensions.NETCore.Setup;
-using Amazon.Runtime;
-using Amazon.SQS;
 using Incremental.Common.Queues.Hosted.Hosted;
 using Incremental.Common.Queues.Hosted.Options;
 using Incremental.Common.Queues.Messages;
-using Incremental.Common.Queues.Messages.Contract;
-using Incremental.Common.Queues.Service;
-using Incremental.Common.Queues.Service.Contract;
-using MediatR;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Incremental.Common.Queues.Hosted
@@ -23,31 +13,27 @@ namespace Incremental.Common.Queues.Hosted
     public static class ServiceCollectionExtensions
     {
         /// <summary>
-        /// Registers a hosted service to handle incoming messages of a specific queue.
+        ///     Registers a hosted service to handle incoming messages of a specific queue.
         /// </summary>
-        /// <returns><see cref="IServiceCollection"/></returns>
+        /// <returns>
+        ///     <see cref="IServiceCollection" />
+        /// </returns>
         public static IServiceCollection AddQueuesHostedServices(this IServiceCollection services, Action<CommonQueuesOptions> hostedOptions)
         {
             var options = new CommonQueuesOptions();
-            
+
             hostedOptions.Invoke(options);
 
-            if (string.IsNullOrWhiteSpace(options.QueueEndpoint))
-            {
-                throw new ArgumentException("QueueEndpoint is a required argument.");
-            }
+            if (string.IsNullOrWhiteSpace(options.QueueEndpoint)) throw new ArgumentException("QueueEndpoint is a required argument.");
 
             if (options.RegisteredMessageTypes.Any() && options.RegisteredMessageTypes.Values.All(t => t.IsAssignableTo(typeof(Message))))
             {
-                
                 foreach (var registeredMessageType in options.RegisteredMessageTypes.Values)
-                {
                     services.AddScoped(typeof(Message), registeredMessageType);
-                }
 
                 services.AddHostedService<QueueHostedService>();
             }
-            
+
             return services;
         }
     }

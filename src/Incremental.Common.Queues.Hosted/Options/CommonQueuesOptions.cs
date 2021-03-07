2ab001a10f7f8a1bc8873d86cccd08f2811019a3
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-07 15:11:00 +0100

    Added docs to CommonQueuesOptions

--- src/Incremental.Common.Queues.Hosted/Options/CommonQueuesOptions.cs
+++ src/Incremental.Common.Queues.Hosted/Options/CommonQueuesOptions.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 
 namespace Incremental.Common.Queues.Hosted.Options
 {
+    /// <summary>
+    /// Common options for hosted queue service.
+    /// </summary>
     public class CommonQueuesOptions
     {
+        /// <summary>
+        /// Queue endpoint.
+        /// </summary>
         public string QueueEndpoint { get; set; }
+        
+        /// <summary>
+        /// Types of messages that this queue will handle.
+        /// </summary>
         public Dictionary<string, Type> RegisteredMessageTypes { get; set; }
     }
 }

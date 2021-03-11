2837e09bd2382501bb5834c46cbff2ddcac3d154
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-11 16:08:21 +0100

    Added ExternalEventHandler

---
+++ src/Incremental.Common.Messaging.Hosted/ExternalEventHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.EventBridge;
+using Amazon.EventBridge.Model;
+using Incremental.Common.Messaging.Hosted.Options;
+using Incremental.Common.Sourcing.Events.Contract;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Incremental.Common.Messaging.Hosted
+{
+    internal class ExternalEventHandler<TExternalEvent> : IEventHandler<TExternalEvent> where TExternalEvent : IExternalEvent
+    {
+        private readonly IAmazonEventBridge _eventBridge;
+        private readonly MessagingOptions _options;
+        private readonly ILogger _logger;
+
+        public ExternalEventHandler(IAmazonEventBridge eventBridge, IOptions<MessagingOptions> options, ILogger logger)
+        {
+            _eventBridge = eventBridge;
+            _logger = logger;
+            _options = options.Value;
+        }
+
+        public async Task Handle(TExternalEvent notification, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await _eventBridge.PutEventsAsync(new PutEventsRequest
+                {
+                    Entries = new List<PutEventsRequestEntry>
+                    {
+                        new()
+                        {
+                            DetailType = notification.GetType().FullName,
+                            EventBusName = _options.EventBus,
+                            Source = Assembly.GetEntryAssembly()?.GetName().Name,
+                            Detail = JsonSerializer.Serialize(notification)
+                        }
+                    }
+                }, cancellationToken);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Unhandled exception sending {@Event} to bus", notification);
+            }
+        }
+    }
+}

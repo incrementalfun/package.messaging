01af71dff5643e22d74fb84acbb544c428b7ce86
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-11 12:32:31 +0100

    Updated Message

--- src/Incremental.Common.Messaging/Message.cs
+++ src/Incremental.Common.Messaging/Message.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using System.Collections.Generic;
+using System.Linq;
+using MediatR;
 
 namespace Incremental.Common.Messaging.Messages
 {
@@ -7,9 +9,43 @@ namespace Incremental.Common.Messaging.Messages
     /// </summary>
     public record Message : IRequest
     {
+        private readonly IList<Message> _innerQueue;
+
+        /// <summary>
+        /// True if there are any messages to be launched as a follow up to this message.
+        /// </summary>
+        public bool HasFollowingSteps => _innerQueue.Any();
+
         /// <summary>
         ///     Receipt of the message in the queue.
         /// </summary>
         public (string Queue, string Id) Receipt { get; init; }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Message()
+        {
+            _innerQueue = new List<Message>();
+        }
+        
+
+        /// <summary>
+        /// Sets up a message to fire if the initial message is a success.
+        /// </summary>
+        /// <param name="message"></param>
+        public void FollowUpWith(Message message)
+        {
+            _innerQueue.Add(message);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Message> FollowingSteps()
+        {
+            return _innerQueue;
+        }
     }
 }

0ae9fd366befaba9f2f62c5d3c8ca669cc3751d6
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-11 23:58:44 +0100

    Added MessageDeserializer service

---
+++ src/Incremental.Common.Messaging.Hosted/Services/IMessageDeserializer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Incremental.Common.Messaging.Hosted.Services
+{
+    internal interface IMessageDeserializer
+    {
+        public bool TryGetType(string typeName, out Type type);
+    }
+}
---
+++ src/Incremental.Common.Messaging.Hosted/Services/MessageDeserializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Incremental.Common.Messaging.Hosted.Services
+{
+    internal class MessageDeserializer : IMessageDeserializer
+    {
+        private readonly Dictionary<string, Type> _supportedTypes;
+
+        public MessageDeserializer(Dictionary<string, Type> supportedTypes)
+        {
+            _supportedTypes = supportedTypes;
+        }
+
+        public bool TryGetType(string typeName, out Type type)
+        {
+            return _supportedTypes.TryGetValue(typeName, out type);
+        }
+    }
+}

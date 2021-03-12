b53ef6d266cc99ba4c4baeb65ed09ed4ab0cd8be
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-12 11:21:04 +0100

    Created IMessagingClientFactory interface

---
+++ src/Incremental.Common.Messaging/Client/IMessagingClientFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+
+namespace Incremental.Common.Messaging.Client
+{
+    /// <summary>
+    /// Factory of messaging clients.
+    /// </summary>
+    public interface IMessagingClientFactory
+    {
+        /// <summary>
+        /// Gets a <see cref="IMessageSender"/> for the provided queue.
+        /// </summary>
+        /// <param name="queue"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public IMessageSender GetSender(string queue, CancellationToken cancellationToken = default);
+        
+        /// <summary>
+        /// Gets a <see cref="IMessageReceiver"/> for the provided queue.
+        /// </summary>
+        /// <param name="queue"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public IMessageReceiver GetReceiver(string queue, CancellationToken cancellationToken = default);
+    }
+}

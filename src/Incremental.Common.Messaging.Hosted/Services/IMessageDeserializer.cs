f3aa4cdf1378c581e81a4d12f2a43d2c19a3bbd1
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-30 10:36:56 +0200

    feat(masstransit): erase old code

--- src/Incremental.Common.Messaging.Hosted/Services/IMessageDeserializer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Incremental.Common.Messaging.Hosted.Services
-{
-    internal interface IMessageDeserializer
-    {
-        public bool TryGetType(string typeName, out Type type);
-    }
-}

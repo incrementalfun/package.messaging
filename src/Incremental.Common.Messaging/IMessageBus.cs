b61bea9118d50de83cebf482fc48a5dde1458d73
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-30 11:03:06 +0200

    feat(bus): add IMessageBus and it's implementation

---
+++ src/Incremental.Common.Messaging/IMessageBus.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Incremental.Common.Messaging
+{
+    public interface IMessageBus
+    {
+        Task Send<TMessage>(TMessage message, CancellationToken cancellationToken = default) where TMessage : Message;
+    }
+}
---
+++ src/Incremental.Common.Messaging/MessageBus.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace Incremental.Common.Messaging
+{
+    public class MessageBus : IMessageBus
+    {
+        private readonly ILogger<MessageBus> _logger;
+        private readonly IBus _internalBus;
+        
+        public MessageBus(ILogger<MessageBus> logger, IBus internalBus)
+        {
+            _logger = logger;
+            _internalBus = internalBus;
+        }
+
+        public async Task Send<TMessage>(TMessage message, CancellationToken cancellationToken = default) where TMessage : Message
+        {
+            await _internalBus.Send(message, cancellationToken);
+        }
+    }
+}

e89b9ca5ba8a63c6c2672064845ab332549bd343
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-08-26 15:45:25 +0300

    feat: add IRequestHandler

---
+++ src/Incremental.Common.Messaging/IRequestHandler.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using MassTransit;
+
+namespace Incremental.Common.Messaging
+{
+    /// <summary>
+    ///     Request handler.
+    /// </summary>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    public interface IRequestHandler<in TRequest, in TResponse> : IConsumer<TRequest> where TRequest : Request<TResponse> where TResponse : class
+    {
+        /// <summary>
+        ///     Respond to a request.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        protected async Task Respond(ConsumeContext<TRequest> context, TResponse response)
+        {
+            await context.RespondAsync<TResponse>(response);
+        }
+    }
+}
